062ba97197658f805e847be9e9beb23f4c6298b0
siddssiddarth <sidds.siddarth01@gmail.com>
2016-07-09 09:38:07 +0530

    .chef/knife.rb~ 	.chef/syntaxcache/ 	Vagrantfile 	Vagrantfile~ 	cookbooks/.gitignore 	cookbooks/.kitchen.yml 	cookbooks/Berksfile 	cookbooks/README.md 	cookbooks/metadata.rb 	cookbooks/my_world/ 	cookbooks/recipes/ 	cookbooks/spec/

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,7 @@
 # See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
-
+cookbook_copyright "sid01"
+cookbook_license "Apache V2"
+cookbook_email "sidds.siddarth01@gmail.com"
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
